16d5a4230ea75c2889ee90d4b4d74ab999294327
zconkle <zconkle@cytracom.com>
2013-07-01 10:12:10 -0500

    updated remote user to work from localhost

--- modules/mysql/mysql_bootstrap.sql
+++ modules/mysql/mysql_bootstrap.sql
@@ -1,2 +1,4 @@
 CREATE USER 'remote'@'%' IDENTIFIED BY 'remote';
 GRANT ALL ON *.* to 'remote'@'%' WITH GRANT OPTION;
+CREATE USER 'remote'@'localhost' IDENTIFIED BY 'remote';
+GRANT ALL ON *.* to 'remote'@'localhost' WITH GRANT OPTION;
