268a1106ab52e8b255f59c6b3b857e43d54854b8
zconkle <zconkle@cytracom.com>
2014-01-23 13:33:54 -0600

    Update init.pp
    Added sox and mpg123 for converting audio files.

--- modules/tools/manifests/init.pp
+++ modules/tools/manifests/init.pp
@@ -1,5 +1,5 @@
 class tools {
-    $packages = ['python-software-properties', 'curl', 'imagemagick']
+    $packages = ['python-software-properties', 'curl', 'imagemagick','mpg321','sox']
     
     package { $packages:
         ensure => installed,
